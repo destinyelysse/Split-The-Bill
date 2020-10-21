e38719c94e7689cb1e6356952fdc78d37648b782
Destiny Serna <destinyeserna@gmail.com>
2020-10-20 22:34:57 -0700

    Initial Commit

---
+++ Split The Bill/Split_The_BillApp.swift
@@ -0,0 +1,17 @@
+//
+//  Split_The_BillApp.swift
+//  Split The Bill
+//
+//  Created by Destiny Serna on 10/20/20.
+//
+
+import SwiftUI
+
+@main
+struct Split_The_BillApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
